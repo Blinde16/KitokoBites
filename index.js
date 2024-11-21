6740505c708bbad19b26637750e5e60ba1713529
porter2003 <156115900+porter2003@users.noreply.github.com>
2024-11-21 16:11:00 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+let express = require("express");
+
+let app = express();
+
+let path = require("path");
+
+const port = 5500;
+// This is an edit by BLAKE, JASON, PORTER, and HUDDY
+
+// means we are working with ejs files so we dont make html files but we write in html with embedded javascript or ejs files
+app.set("view engine", "ejs");
+
+// refers to the views folder
+app.set("views", path.join(__dirname, "views"));
+
+// makes it so you can pull stuff from the req.body
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.static('public'));
+
+//connects us to our server in postgres
+const knex = require("knex") ({
+    client : "pg",
+    connection : {
+        host : "localhost",
+        user : "postgres",
+        password : "admin",
+        database : "mycompany",
+        port : 5432
+    }
+})
+
+
+app.get("/", (req,res) => {
+    res.render("index")
+})
+
+app.post("/login", (req,res) => {
+    const { username, password } = req.body;
+    
+})
+
+app.get("/project", (req,res) => {
+    res.render("project")
+})
+app.get("/signup", (req,res) =>{
+    res.render("signup")
+})
+
+app.post('/storeLogin', (req, res) => {
+    const { username, email, password } = req.body;
+    console.log(username + ' ' + email + ' ' + password)
+    // Logic to store account info (e.g., saving to a database)
+    res.render(`createdAccount`, {user: username});
+  });
+
+app.listen(port, console.log('Server listening'))
